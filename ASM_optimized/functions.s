7cb0efbcc64cf39db73f20aa180c717cbbbee760
alexnurin <an362@mail.ru>
2022-11-28 00:24:10 +0300

    оптимизация строк

--- ASM_optimized/functions.s
+++ ASM_optimized/functions.s
@@ -38,21 +38,17 @@ function:                               # xmm0 = x
 	mulsd	xmm1, xmm1                  # xmm1 := x^2
     movapd	xmm2, xmm1                  # xmm2 := x^2
 	mulsd	xmm2, xmm0                  # xmm2 = x^3
-
 	movsd	xmm3, QWORD PTR .LC0[rip]   # xmm3 := 0.5
 	mulsd	xmm3, xmm1                  # xmm3 = x^2 * 0.5
 	subsd	xmm2, xmm3                  # xmm2 = x^3 - x^2 * 0.5
-
 	movsd	xmm3, QWORD PTR .LC1[rip]   # xmm3 := 0.2
 	mulsd	xmm3, xmm0                  # xmm3 = x * 0.2
 	addsd	xmm2, xmm3                  # xmm2 = x^3 - x^2 * 0.5 + x * 0.2
-
 	movsd	xmm1, QWORD PTR .LC2[rip]   # xmm1 := 4
 	movapd  xmm0, xmm2
 	subsd	xmm0, xmm1                  # xmm0 = x^3 - x^2 * 0.5 + x * 0.2 - 4
 	pop	rbp                             # f(x)
 	ret
-
 	.size	function, .-function
 	.globl	find_zero
 	.type	find_zero, @function
@@ -102,48 +98,3 @@ find_zero:
 	movsd	xmm0, QWORD PTR -16[rbp]        # xmm0 := r
 	leave
 	ret                                     # return r
-
-	.size	find_zero, .-find_zero
-	.section	.rodata
-	.align 8
-.LC0:
-	.long	0
-	.long	1071644672
-	.align 8
-.LC1:
-	.long	2576980378
-	.long	1070176665
-	.align 8
-.LC2:
-	.long	0
-	.long	1074790400
-	.align 8
-.LC3:
-	.long	0
-	.long	1072693248
-	.align 8
-.LC4:
-	.long	0
-	.long	1074266112
-	.align 8
-.LC5:
-	.long	0
-	.long	1073741824
-	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
