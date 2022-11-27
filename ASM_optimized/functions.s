2dd8cf1512e5945230f60e631726d5ef2348148c
alexnurin <an362@mail.ru>
2022-11-28 00:36:17 +0300

    багфикс

--- ASM_optimized/functions.s
+++ ASM_optimized/functions.s
@@ -98,3 +98,47 @@ find_zero:
 	movsd	xmm0, QWORD PTR -16[rbp]        # xmm0 := r
 	leave
 	ret                                     # return r
+	.size	find_zero, .-find_zero
+	.section	.rodata
+	.align 8
+.LC0:
+	.long	0
+	.long	1071644672
+	.align 8
+.LC1:
+	.long	2576980378
+	.long	1070176665
+	.align 8
+.LC2:
+	.long	0
+	.long	1074790400
+	.align 8
+.LC3:
+	.long	0
+	.long	1072693248
+	.align 8
+.LC4:
+	.long	0
+	.long	1074266112
+	.align 8
+.LC5:
+	.long	0
+	.long	1073741824
+	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
